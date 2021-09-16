5ca7ba3dde91513becf7c6d6071ca924fd51f13f
Milica <milakovicmilica@yahoo.com>
2021-09-17 00:53:01 +0200

    Moving player and setting obstacles

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,35 +22,117 @@ namespace RocketMan
     /// </summary>
     public partial class MainWindow : Window
     {
-        int left = 0;
-        int speed = 5;
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        Random random = new Random();
+
+        bool goLeft, goRight;
+
+        ImageBrush playerImage = new ImageBrush();
+        ImageBrush starImage = new ImageBrush();
+
+        int speed = 10;
+        int playerSpeed = 8;
+
 
 
         public MainWindow()
         {
             InitializeComponent();
 
-            var timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(10);
-            timer.Start();
-            timer.Tick += _timer_Tick;
+            myCanvas.Focus();
+
+            gameTimer.Tick += GameLoop;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+
+            StartGame();
 
         }
 
-        void _timer_Tick(object sender, EventArgs e)
+        private void GameLoop(object sender, EventArgs e)
         {
-            left += speed;
-            BlueCircle.Margin = new Thickness(left, 0, 0, 90);
 
-            if(left > 443)
+            // pomjeranje rakete
+
+            if(goLeft == true && Canvas.GetLeft(Player) > 0)
             {
-                speed = -5;
+                Canvas.SetLeft(Player, Canvas.GetLeft(Player) - playerSpeed);
             }
-            if (left < 2)
+            if(goRight == true && Canvas.GetLeft(Player) + 150 < Application.Current.MainWindow.Width)
             {
-                speed = 5;
+                Canvas.SetLeft(Player, Canvas.GetLeft(Player) + playerSpeed);
             }
 
+
+            // postavljanje prepreka
+            foreach(var x  in myCanvas.Children.OfType<Rectangle>())
+            {
+                if((string) x.Tag == "Planet")
+                {
+                    Canvas.SetTop(x, Canvas.GetTop(x) + speed);
+
+                    if (Canvas.GetTop(x) > 500)
+                    {
+                        Canvas.SetTop(x, random.Next(100, 400) * -1);
+                        Canvas.SetLeft(x, random.Next(0, 430));
+
+                        ChangePlanets(x);
+                    }
+                }
+            }
+
+
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Left)
+            {
+                goLeft = false;
+            }
+            if(e.Key == Key.Right)
+            {
+                goRight = false;
+            }
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                goLeft = true;
+
+            }
+            if (e.Key == Key.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void StartGame()
+        {
+            speed = 8;
+            gameTimer.Start();
+
+            goLeft = false;
+            goRight = false;
+
+            playerImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/resources/player.png"));
+            starImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/resources/star.png"));
+
+            playerImage.Stretch = Stretch.UniformToFill;
+            Player.Fill = playerImage;
+
+        }
+
+        private void ChangePlanets(Rectangle planet)
+        {
+            int planetNumber = random.Next(1, 10);
+            ImageBrush planetImage = new ImageBrush();
+
+            planetImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/resources/planet" + planetNumber + ".png"));
+            planetImage.Stretch = Stretch.UniformToFill;
+            planet.Fill = planetImage;
         }
     }
 }
